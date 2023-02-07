cf66b4044af95dd6d437701ea24bfc3cbd05ac38
Emily Nixon <123026549+enixon01@users.noreply.github.com>
2023-02-06 23:31:05 -0500

    Add files via upload

---
+++ CPSC HW1.py
@@ -0,0 +1,34 @@
+#Emily Nixon
+#1
+type ("Rosewater")
+type (0.001)
+type (37)
+type (False)
+#2
+#First, I will demonstrate the plus symbol in code. The plus symbol puts two arguments togethe
+print ("Hello" + "professor")
+#Second, I will demonstrate the comma symbol in code. The comma symbol puts a break in the line to input a variable. For example,
+import math
+x = math.pi
+print ("The value of pi is", x)
+#3
+#First we learned the integer function, which converts its argument to an integer
+int (2.23)
+#Then we learned the float function, which converts its argumenet to a float number
+float (2)
+#Then we learned the string function, which converts its argument to a string
+str (2)
+#Finally, we learned the bool function, which converts its argument to true or false
+bool (1)
+#4
+#We would use the backslash n function separated by plus symbols
+print ("This\n"+"function\n"+"is\n"+"five\n"+"lines.")
+#5
+flour = 2
+milk = 1
+egg = 4
+oil = 0.4
+vanilla = 0.012
+cake = 2 * flour + 0.5 * (milk - egg + vanilla) + oil**2
+print (cake)
+
